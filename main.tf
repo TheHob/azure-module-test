d8956dec95952433412b0ea96ac04044e09e902a
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2018-07-23 12:03:22 -0400

    Update main.tf
    changing hostname.

--- main.tf
+++ main.tf
@@ -21,12 +21,11 @@ module "windowsserver" {
   source              = "Azure/compute/azurerm"
   version             = "1.1.5"
   location            = "${var.location}"
-  vm_hostname         = "chad-ptfe-1"
+  vm_hostname         = "arslan-ptfe-1"
   admin_password      = "${var.admin_password}"
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["${var.windows_dns_prefix}"]
   vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  count = "2"
 }
 
 module "network" {
